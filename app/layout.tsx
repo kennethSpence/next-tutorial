0736c97ba915b8a9c02056c3367a84be5695f0a1
Kenneth Spence <kenneth.spence1@gmail.com>
2024-12-21 16:13:06 -0800

    enable speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "@/app/ui/global.css";
 import { inter } from "./ui/fonts";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function RootLayout({
   children,
